3ec2aabd2c916e644046f443a398e1c453218f35
swatibyajbook <Swatimandal01@gmail.com>
2022-08-08 19:24:11 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
+import Login from "./Login";
+import Footer from "./Footer";
+
+
+function App() {
+ 
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+            <Footer />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
